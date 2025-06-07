62d36c1dbb010f8fdab9cdb57bbf4bbc4f53a84f
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-06-07 14:07:01 -0600

    added search bar

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import ProjectCard from "./ProjectCard";
+
 
 //child of Projects
 export default function ProjectList({projects}) {
+
+	//search bar
+	const [searchTerm, setSearchTerm] = useState('');
+	const [filteredProjects, setFilteredProjects] = useState(projects);
+
+	const handleSearch = (event) => {
+		
+		const term = event.target.value;
+		setSearchTerm(term);
+		
+		const filtered = projects.filter((project) =>
+        project.name.toLowerCase().includes(term.toLowerCase())
+         );
+        setFilteredProjects(filtered);
+    };
+
 	return(
 		<div>
+			<label>Search Projects:</label>
+			 <input
+        		type="text"
+        		placeholder="Search projects..."
+        		value={searchTerm}
+        		onChange={handleSearch}
+      		/>
+
+
 			<h2>Project List</h2>
-			{projects.map((project) => (
+			{filteredProjects.map((project) => (
 				<ProjectCard key={project.id} project={project} />
 			))}
 		</div>
