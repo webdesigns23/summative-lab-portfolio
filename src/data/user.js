45434b0e18dee0b931251f6bfbe2f61f41dd3006
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-05 21:15:28 -0600

    updated profile picture

--- src/data/user.js
+++ src/data/user.js
@@ -2,7 +2,7 @@ const user = {
   name: "Sharmaine Perea",
   city: "Denver, Colorado",
   bio: "Welcome to my frontend development project portfolio! These projects demonstrate my skills in building responsive, interactive user interfaces using HTML, CSS, and JavaScript, with powerful functionality powered by React. Take a look at my projects below to see what I have created. You will notice that I have also added a form to seamlessly add in new projects to my list to ensure my page is current and up to date. As well as filter to easily search through my project list.",
-  image: "https://raw.githubusercontent.com/learn-co-curriculum/react-hooks-hogwild/master/public/images/piglet.jpg",
+  image:"./Images/headshot1.jpg"
   };
 
 
